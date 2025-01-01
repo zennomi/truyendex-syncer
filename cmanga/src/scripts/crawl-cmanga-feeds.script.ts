f90fbcf3c1005f75754c3ba9bf8e939f260d28af
zen-lenovo <zennomi.kun@gmail.com>
2025-01-01 19:16:57 +0700

    refactor

--- cmanga/src/scripts/crawl-cmanga-feeds.script.ts
+++ cmanga/src/scripts/crawl-cmanga-feeds.script.ts
@@ -1,5 +1,10 @@
 import mongoose from "mongoose";
-import { mongooseWrapper, parseJsonFromPage, realBrowser } from "@/utils";
+import {
+  getJsonData,
+  mongooseWrapper,
+  parseJsonFromPage,
+  realBrowser,
+} from "@/utils";
 
 // sudo apt-get install xvfb
 // warp
@@ -13,20 +18,14 @@ const main = async () => {
   let paramsPage = 1;
   const limit = 1000;
   while (true) {
-    await page.goto(
-      `https://cmangam.com/api/home_album_list?file=image&num_chapter=0&sort=update&type=new&tag=all&limit=${limit}&page=${paramsPage}`,
-      {
-        waitUntil: "networkidle2",
-      }
-    );
-
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-
     // Extract the JSON content
-    const jsonData = await parseJsonFromPage<{
+    const jsonData = await getJsonData<{
       data: { id_album: string; info: string }[];
       total: number;
-    }>(page);
+    }>(
+      `https://cmangam.com/api/home_album_list?file=image&num_chapter=0&sort=update&type=new&tag=all&limit=${limit}&page=${paramsPage}`,
+      page
+    );
 
     const result = await collection.bulkWrite(
       jsonData.data
--- cmanga/src/utils/puppeteer.ts
+++ cmanga/src/utils/puppeteer.ts
@@ -23,3 +23,17 @@ export async function parseJsonFromPage<T>(page: PageWithCursor) {
     return JSON.parse(document.body.innerText);
   })) as T;
 }
+
+export async function getJsonData<T>(url: string, page: PageWithCursor) {
+  await page.goto(url, {
+    waitUntil: "networkidle2",
+  });
+
+  // delay 5s
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  // Extract the JSON content
+  const jsonData = await parseJsonFromPage<T>(page);
+
+  return jsonData;
+}
